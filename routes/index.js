c1f88e09c5a2260eb64c49b34f13ceaa30cf0e4e
sixyougo <one-p-0723.you@docomo.ne.jp>
2022-08-11 17:04:13 +0900

    会員登録画面1

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* GET home page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
 module.exports = router;
