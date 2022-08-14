9d677c77b2eecb88db461a7b40d4f0e9363636de
sixyougo <one-p-0723.you@docomo.ne.jp>
2022-08-14 15:24:58 +0900

    修正

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
 module.exports = router;
